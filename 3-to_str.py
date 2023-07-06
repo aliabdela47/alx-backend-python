5b7d244fe593a62484464db87ceed54ee3abc0ad
aliabdela47 <ardii.2000@gmail.com>
2023-07-06 19:23:38 +0000

    fix2

--- 3-to_str.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Write a type-annotated function to_str that takes a float n
-as argument and returns the string representation of the float.
-"""
-
-
-def to_str(n: float) -> str:
-    """Returns the string representation of the float"""
-    return str(n)
